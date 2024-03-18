139459e4f06c762b9aa7dc2516a5ba737ba1e26f
Laurent-alexandre CADINOT <lcadinot@paul-f4Br4s6.clusters.42paris.fr>
2024-03-18 18:55:37 +0100

    add

--- Mandatory/Sources/main.c
+++ Mandatory/Sources/main.c
@@ -60,6 +60,277 @@ int	main(int ac, char **av)
 	free_all(&game);
 	return (0);
 }
+// #include <stdio.h>
+// #include <math.h>
+
+// #define WIDTH 800
+// #define HEIGHT 600
+// #define FOV_ANGLE M_PI / 3.0 // Angle de champ de vision (60 degrés)
+// #define PLAYER_HEIGHT 32 // Hauteur du joueur
+// #define NUM_RAYS WIDTH // Nombre de rayons (colonnes de pixels)
+
+// typedef struct {
+//     double x;
+//     double y;
+// } Vector2D;
+
+// typedef struct {
+//     Vector2D start;
+//     Vector2D end;
+// } Wall;
+
+// typedef struct {
+//     Vector2D pos;
+//     double angle;
+// } Player;
+
+// Player player = {{WIDTH / 2.0, HEIGHT / 2.0}, M_PI / 3.0}; // Position et angle de départ du joueur
+// int key_state[280] = {0}; // Tableau pour stocker l'état des touches (appuyée ou non)
+
+// double distance(Vector2D p1, Vector2D p2) {
+//     return sqrt(pow(p2.x - p1.x, 2) + pow(p2.y - p1.y, 2));
+// }
+
+// void movePlayer(double step) {
+//     player.pos.x += step * cos(player.angle);
+//     player.pos.y += step * sin(player.angle);
+// }
+
+// void rotatePlayer(double angle) {
+//     player.angle += angle;
+//     if (player.angle >= 2 * M_PI) player.angle -= 2 * M_PI;
+//     if (player.angle < 0) player.angle += 2 * M_PI;
+// }
+
+// void renderScene(void *mlx_ptr, void *win_ptr) {
+//     mlx_clear_window(mlx_ptr, win_ptr); // Effacer la fenêtre
+
+//     Wall wall = {{300, 200}, {300, 400}}; // Mur en première personne
+
+//     // Dessiner les murs en perspective
+//     for (int i = 0; i < NUM_RAYS; i++) {
+//         // Angle du rayon
+//         double ray_angle = player.angle + (double)i / NUM_RAYS - FOV_ANGLE / 2.0;
+
+//         // Distance du mur par rapport au joueur
+//         double wall_distance = distance(player.pos, wall.start);
+
+//         // Angle du mur par rapport à l'axe x
+//         double wall_angle = atan2(wall.end.y - wall.start.y, wall.end.x - wall.start.x);
+
+//         // Différence d'angle entre le joueur et le mur
+//         double angle_diff = ray_angle - wall_angle;
+
+//         // Distance perpendiculaire entre le joueur et le mur
+//         double perpendicular_distance = wall_distance * cos(angle_diff);
+
+//         // Hauteur du mur à dessiner
+//         double wall_height = HEIGHT / perpendicular_distance * PLAYER_HEIGHT;
+
+//         // Position verticale de début et de fin du mur à dessiner
+//         double wall_start_y = (HEIGHT - wall_height) / 2.0;
+//         double wall_end_y = wall_start_y + wall_height;
+
+//         // Dessiner le mur
+//         int color = 0xFFFFFF; // Couleur du mur (blanc)
+//         for (int y = wall_start_y; y < wall_end_y; y++) {
+//             mlx_pixel_put(mlx_ptr, win_ptr, i, y, color); // Dessiner le mur
+//         }
+//     }
+// }
+
+// int keyPressEvent(int keycode, void *param) {
+//     key_state[keycode] = 1;
+
+//     // Déplacement et rotation du joueur en fonction des touches pressées
+//     double step = 5.0; // Pas de déplacement
+//     double rotate_angle = M_PI / 16.0; // Angle de rotation
+//     switch (keycode) {
+//         case 119: // Touche W (avancer)
+//             movePlayer(step);
+//             break;
+//         case 115: // Touche S (reculer)
+//             movePlayer(-step);
+//             break;
+//         case 97: // Touche A (tourner à gauche)
+//             rotatePlayer(-rotate_angle);
+//             break;
+//         case 100: // Touche D (tourner à droite)
+//             rotatePlayer(rotate_angle);
+//             break;
+//     }
+
+//     return 0;
+// }
+
+// int keyReleaseEvent(int keycode, void *param) {
+//     key_state[keycode] = 0;
+//     return 0;
+// }
+
+// int main() {
+//     void *mlx_ptr;
+//     void *win_ptr;
+
+//     // Initialisation de la connexion à la fenêtre
+//     mlx_ptr = mlx_init();
+//     if (!mlx_ptr) {
+//         printf("Erreur: Impossible d'initialiser MinilibX.\n");
+//         return 1;
+//     }
+
+//     // Création de la fenêtre
+//     win_ptr = mlx_new_window(mlx_ptr, WIDTH, HEIGHT, "Ray Tracing");
+
+//     // Association des hooks pour la gestion des événements de touche
+//     mlx_hook(win_ptr, 2, 0, keyPressEvent, NULL); // KeyPress
+//     mlx_hook(win_ptr, 3, 0, keyReleaseEvent, NULL); // KeyRelease
+
+//     // Affichage initial de la scène
+//     renderScene(mlx_ptr, win_ptr);
+
+//     // Boucle principale pour maintenir la fenêtre ouverte
+//     mlx_loop(mlx_ptr);
+
+//     return 0;
+// }
+
+
+// #include "mlx.h"
+// #include <stdio.h>
+// #include <math.h>
+
+// #define WIDTH 800
+// #define HEIGHT 600
+// #define FOV_ANGLE M_PI / 3.0 // Angle de champ de vision (60 degrés)
+// #define PLAYER_HEIGHT 32 // Hauteur du joueur
+// #define NUM_RAYS WIDTH // Nombre de rayons (colonnes de pixels)
+
+// typedef struct {
+//     double x;
+//     double y;
+// } Vector2D;
+
+// typedef struct {
+//     Vector2D start;
+//     Vector2D end;
+// } Wall;
+
+// typedef struct {
+//     Vector2D pos;
+//     double angle;
+// } Player;
+
+// Player player = {{WIDTH / 2.0, HEIGHT / 2.0}, M_PI / 3.0}; // Position et angle de départ du joueur
+// int key_state[280] = {0}; // Tableau pour stocker l'état des touches (appuyée ou non)
+
+// double distance(Vector2D p1, Vector2D p2) {
+//     return sqrt(pow(p2.x - p1.x, 2) + pow(p2.y - p1.y, 2));
+// }
+
+// void movePlayer(double step) {
+//     player.pos.x += step * cos(player.angle);
+//     player.pos.y += step * sin(player.angle);
+// }
+
+// void rotatePlayer(double angle) {
+//     player.angle += angle;
+//     if (player.angle >= 2 * M_PI) player.angle -= 2 * M_PI;
+//     if (player.angle < 0) player.angle += 2 * M_PI;
+// }
+
+// void renderScene(void *mlx_ptr, void *win_ptr) {
+//     mlx_clear_window(mlx_ptr, win_ptr); // Effacer la fenêtre
+
+//     Wall wall = {{300, 200}, {300, 400}}; // Mur en première personne
+
+//     // Dessiner les murs en perspective
+//     for (int i = 0; i < NUM_RAYS; i++) {
+//         // Angle du rayon
+//         double ray_angle = player.angle + (double)i / NUM_RAYS - FOV_ANGLE / 2.0;
+
+//         // Distance du mur par rapport au joueur
+//         double wall_distance = distance(player.pos, wall.start);
+
+//         // Angle du mur par rapport à l'axe x
+//         double wall_angle = atan2(wall.end.y - wall.start.y, wall.end.x - wall.start.x);
+
+//         // Différence d'angle entre le joueur et le mur
+//         double angle_diff = ray_angle - wall_angle;
+
+//         // Distance perpendiculaire entre le joueur et le mur
+//         double perpendicular_distance = wall_distance * cos(angle_diff);
+
+//         // Hauteur du mur à dessiner
+//         double wall_height = HEIGHT / perpendicular_distance * PLAYER_HEIGHT;
+
+//         // Position verticale de début et de fin du mur à dessiner
+//         double wall_start_y = (HEIGHT - wall_height) / 2.0;
+//         double wall_end_y = wall_start_y + wall_height;
+
+//         // Dessiner le mur
+//         int color = 0xFFFFFF; // Couleur du mur (blanc)
+//         for (int y = wall_start_y; y < wall_end_y; y++) {
+//             mlx_pixel_put(mlx_ptr, win_ptr, i, y, color); // Dessiner le mur
+//         }
+//     }
+// }
+
+// int keyPressEvent(int keycode, void *param) {
+//     key_state[keycode] = 1;
+
+//     // Déplacement et rotation du joueur en fonction des touches pressées
+//     double step = 5.0; // Pas de déplacement
+//     double rotate_angle = M_PI / 16.0; // Angle de rotation
+//     switch (keycode) {
+//         case 119: // Touche W (avancer)
+//             movePlayer(step);
+//             break;
+//         case 115: // Touche S (reculer)
+//             movePlayer(-step);
+//             break;
+//         case 97: // Touche A (tourner à gauche)
+//             rotatePlayer(-rotate_angle);
+//             break;
+//         case 100: // Touche D (tourner à droite)
+//             rotatePlayer(rotate_angle);
+//             break;
+//     }
+
+//     return 0;
+// }
+
+// int keyReleaseEvent(int keycode, void *param) {
+//     key_state[keycode] = 0;
+//     return 0;
+// }
+
+// int main() {
+//     void *mlx_ptr;
+//     void *win_ptr;
+
+//     // Initialisation de la connexion à la fenêtre
+//     mlx_ptr = mlx_init();
+//     if (!mlx_ptr) {
+//         printf("Erreur: Impossible d'initialiser MinilibX.\n");
+//         return 1;
+//     }
+
+//     // Création de la fenêtre
+//     win_ptr = mlx_new_window(mlx_ptr, WIDTH, HEIGHT, "Ray Tracing");
+
+//     // Association des hooks pour la gestion des événements de touche
+//     mlx_hook(win_ptr, 2, 0, keyPressEvent, NULL); // KeyPress
+//     mlx_hook(win_ptr, 3, 0, keyReleaseEvent, NULL); // KeyRelease
+
+//     // Affichage initial de la scène
+//     renderScene(mlx_ptr, win_ptr);
+
+//     // Boucle principale pour maintenir la fenêtre ouverte
+//     mlx_loop(mlx_ptr);
+
+//     return 0;
+// }
 
 
 
